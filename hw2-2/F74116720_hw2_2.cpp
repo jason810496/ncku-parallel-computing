3ae0bf3dc00f5cb4f35d529c4c4d82329d42ded4
jason810496 <zhu424.dev@gmail.com>
2024-10-22 20:47:43 +0800

    feat: hw2-2 v2: split by worker for all node

--- hw2-2/F74116720_hw2_2.cpp
+++ hw2-2/F74116720_hw2_2.cpp
@@ -8,39 +8,40 @@ O(n^2) Dijkstra's algorithm with MPI
 #include<vector>
 #include<utility>
 #include<queue>
+#include<string.h>
 
-#define MAX_N 50005
+#define MAX_N 50001
 #define F first
 #define S second
 #define INF 1e9
 
-struct Edge{
-    short t,w;
-    Edge(short t,short w):t(t),w(w){}
-    Edge(){}
-
-    bool operator>(const Edge& e) const{
-        return w > e.w;
+void dijksra(int start_node,int end_node,short graph[MAX_N][MAX_N],short local_dis[MAX_N]){
+    std::priority_queue<std::pair<int,int>,std::vector<std::pair<int,int>>,std::greater<std::pair<int,int>>> pq;
+    pq.push({0,start_node});
+    while(!pq.empty()){
+        auto [d,u] = pq.top();
+        pq.pop();
+        if(d > local_dis[u]) continue;
+        for(int v=0;v<MAX_N;v++){
+            if(graph[u][v] == 0) continue;
+            if(local_dis[v] > local_dis[u] + graph[u][v]){
+                local_dis[v] = local_dis[u] + graph[u][v];
+                pq.push({local_dis[v],v});
+            }
+        }
     }
-};
+}
 
 
 int main(int argc, char *argv[]){
     int n;
     int cluster_size;
     int worker_id;
+    short graph[MAX_N][MAX_N];
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &cluster_size);
     MPI_Comm_rank(MPI_COMM_WORLD, &worker_id);
-    // MPI Data Type
-    MPI_Datatype MPI_EDGE_TYPE;
-    MPI_Type_contiguous(2,MPI_SHORT,&MPI_EDGE_TYPE);
-    MPI_Type_commit(&MPI_EDGE_TYPE);
 
-    vector< Edge > graph[MAX_N];
-    vector< int > dis(MAX_N,INF);
-    vector< bool > vis(MAX_N,false);
-    priority_queue<Edge, vector<Edge>, greater<Edge>> pq;
     // input
     if(worker_id == 0){
         // file input
@@ -56,31 +57,60 @@ int main(int argc, char *argv[]){
         for(int i=0;i<n;i++){
             short from,to,weight;
             fscanf(fp,"%hd %hd %hd",&from,&to,&weight);
-            graph[from].push_back(Edge(to,weight));
+            graph[from][to] = weight;
         }
         fclose(fp);
+        // init data
+        memset(graph,0,sizeof(graph));
     }
     // broadcast data
     MPI_Bcast(&n,1,MPI_INT,0,MPI_COMM_WORLD);
-    MPI_Bcast(graph.data(),MAX_N,MPI_EDGE_TYPE,0,MPI_COMM_WORLD);
-    // Dijkstra
-    short start_vertex = 0;
-    dis[start_vertex] = 0;
+    MPI_Bcast(graph,MAX_N*MAX_N,MPI_SHORT,0,MPI_COMM_WORLD);
+    // divide the load
+    int chunk_size = n / cluster_size;
+    int start = worker_id * chunk_size;
+    int end = (worker_id == cluster_size-1)? n: start + chunk_size;
 
-    while (!pq.empty()) {
-        int u = pq.top().second; pq.pop();
-        if (vis[u]) continue;
-        vis[u] = true;
-        // split all adj vertexes to workers
-        // for (Edge e : adj[u]) {
-        //     if (e.t < t || dist[e.v] <= dist[u] + e.w) continue;
-        //     dist[e.v] = dist[u] + e.w;
-        //     pq.push({dist[e.v], e.v});
-        // }
+    // init local_dis for all workers
+    short local_dis[MAX_N];
+    for(int i=0;i<n;i++){
+        local_dis[i] = INF;
+    }
+    local_dis[0] = 0;
+    dijksra(start_node,end_node,graph,local_dis);
+
+    // Dijkstra
+    if( worker_id == 0 ){
+        // init global_dis
+        short global_dis[MAX_N];
+        for(int i=0;i<n;i++){
+            global_dis[i] = INF;
+        }
+        global_dis[0] = 0;
+        // self result
+        for(int i=0;i<n;i++){
+            global_dis[i] = std::min(global_dis[i],local_dis[i]);
+        }
+        // merge the results 
+        for(int i=1;i<cluster_size;i++){
+            short recv_dis[MAX_N];
+            MPI_Recv(recv_dis,MAX_N,MPI_SHORT,i,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+            for(int j=0;j<n;j++){
+                global_dis[j] = std::min(global_dis[j],recv_dis[j]);
+            }
+        }
+    }
+    else{
+        // send the results to the worker 0
+        MPI_Send(local_dis,MAX_N,MPI_SHORT,0,0,MPI_COMM_WORLD);
     }
 
+    // print the result
+    if(worker_id == 0){
+        for(int i=0;i<n;i++){
+            printf("%d ",global_dis[i]);
+        }
+    }
 
-    MPI_Type_free(&MPI_EDGE_TYPE);
-    MPI_Finalize();
     return 0;
 }
